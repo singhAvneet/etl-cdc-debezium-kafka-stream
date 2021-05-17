387060841b204ed05d4474f4b5ea6256121d4426
singhAvneet <avneet_cochhar@hotmail.com>
2021-05-17 15:55:40 -0600

    Update setup.sql

--- Setup/setup.sql
+++ Setup/setup.sql
@@ -1,8 +1,8 @@
 USE [master]
 GO
-CREATE LOGIN [debezium] WITH PASSWORD=N'Debetest01', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+CREATE LOGIN [debezium] WITH PASSWORD='whatever', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
 GO
-USE [CIMS]
+USE [DB]
 GO
 CREATE USER [debezium] FOR LOGIN [debezium]
 GO
@@ -12,152 +12,16 @@ ALTER ROLE [db_owner] ADD MEMBER [debezium]
 GO
 
 -- Enable CDC on DB --
-USE CIMS
+USE DB
 GO
 EXEC sys.sp_cdc_enable_db
 GO
 
--- Enable CDC per table --
-USE CIMS
-GO
-
-EXEC sys.sp_cdc_enable_table        
-@source_schema = N'Financial',
-@source_name   = N'Claim',
-@role_name     = N'debezium'--,
-GO
-
 EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimCostPlus',
-@role_name     = N'debezium'--,
+    @source_schema = N'Person',
+    @source_name   = N'Address',
+    @role_name     = NULL,
+    @supports_net_changes = 1
 GO
 
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Customer',
-@source_name   = N'ClaimBlackList',
-@role_name     = N'debezium'--,
-GO
 
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'AdjustingClaimLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimCase',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimContractLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimDrug',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimEscAccumDetail',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimNoteLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimRefund',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimStatus',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimStatusClaimLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ClaimVoucherNoteClaimLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'ESIClaimImportDetail',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'TransactionClaimLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Financial',
-@source_name   = N'TravelAssistClaimCaseLink',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ClaimContractRelationship',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ClaimOverrideReason',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ClaimRefundReason',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ClaimState',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ClaimVoucherNote',
-@role_name     = N'debezium'--,
-GO
-
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ESIClaimFileRecord',
-@role_name     = N'debezium'--,
-GO
-EXEC sys.sp_cdc_enable_table
-@source_schema = N'Reference',
-@source_name   = N'ESIClaimImportTypeDetail',
-@role_name     = N'debezium'--,
-GO
-
--- Display affected tables in table view --
-EXEC sys.sp_cdc_help_change_data_capture
-GO
